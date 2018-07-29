7faa75ba9185dd0fb1314cf95393374804cee14c
Yechan Bae <qwazpia@gmail.com>
2018-07-29 22:52:38 +0900

    Add subcommand to owl-cli

--- owl-cli/src/main.rs
+++ owl-cli/src/main.rs
@@ -17,11 +17,15 @@ fn main() {
             .value_name("TOML")
             .help("Sets a custom config file (default is \"config.toml\"")
             .takes_value(true))
+        .subcommand(SubCommand::with_name("test")
+            .about("test basic connection"))
         .get_matches();
 
     let config = matches.value_of("config").unwrap_or("config.toml");
     println!("Value for config: {}", config);
 
-    let client = SyncClient::connect("localhost:5959", client::Options::default()).unwrap();
-    println!("{}", client.hello("Mom".to_string()).unwrap());
+    if let Some(_matches) = matches.subcommand_matches("test") {
+        let client = SyncClient::connect("localhost:5959", client::Options::default()).unwrap();
+        println!("{}", client.hello("Mom".to_string()).unwrap());
+    }
 }
