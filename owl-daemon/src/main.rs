277b1165c4ad14775a6d741ffc80082d4b70f6d4
Yechan Bae <qwazpia@gmail.com>
2018-07-29 22:06:27 +0900

    Setup tarpc

--- owl-daemon/src/main.rs
+++ owl-daemon/src/main.rs
@@ -1,3 +1,36 @@
+#![feature(plugin, use_extern_macros, proc_macro_path_invoc)]
+#![plugin(tarpc_plugins)]
+
+#[macro_use]
+extern crate tarpc;
+
+use std::sync::mpsc;
+use std::thread;
+use tarpc::sync::{client, server};
+use tarpc::sync::client::ClientExt;
+use tarpc::util::{FirstSocketAddr, Never};
+
+service! {
+    rpc hello(name: String) -> String;
+}
+
+#[derive(Clone)]
+struct HelloServer;
+
+impl SyncService for HelloServer {
+    fn hello(&self, name: String) -> Result<String, Never> {
+        Ok(format!("Hello, {}!", name))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let mut handle = HelloServer.listen("localhost:0", server::Options::default())
+            .unwrap();
+        tx.send(handle.addr()).unwrap();
+        handle.run();
+    });
+    let client = SyncClient::connect(rx.recv().unwrap(), client::Options::default()).unwrap();
+    println!("{}", client.hello("Mom".to_string()).unwrap());
 }
