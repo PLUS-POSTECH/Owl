d1e768fa66bd8710240a465cfcd925d70ed51af8
Yechan Bae <qwazpia@gmail.com>
2018-07-29 22:34:18 +0900

    Separate tarpc lib and bin

--- owl-daemon/src/lib.rs
+++ owl-daemon/src/lib.rs
@@ -1,3 +1,8 @@
+#![feature(plugin, use_extern_macros, proc_macro_path_invoc)]
+#![plugin(tarpc_plugins)]
+
+// Diesel ORM
+/*
 #[macro_use]
 extern crate diesel;
 extern crate chrono;
@@ -6,3 +11,23 @@ pub mod schema;
 pub mod models;
 
 use diesel::prelude::*;
+*/
+
+// tarpc
+#[macro_use]
+extern crate tarpc;
+
+use tarpc::util::Never;
+
+service! {
+    rpc hello(name: String) -> String;
+}
+
+#[derive(Clone)]
+pub struct HelloServer;
+
+impl SyncService for HelloServer {
+    fn hello(&self, name: String) -> Result<String, Never> {
+        Ok(format!("Hello, {}!", name))
+    }
+}
--- owl-daemon/src/main.rs
+++ owl-daemon/src/main.rs
@@ -1,36 +1,27 @@
-#![feature(plugin, use_extern_macros, proc_macro_path_invoc)]
-#![plugin(tarpc_plugins)]
-
-#[macro_use]
+extern crate owl_daemon;
 extern crate tarpc;
 
 use std::sync::mpsc;
 use std::thread;
-use tarpc::sync::{client, server};
-use tarpc::sync::client::ClientExt;
-use tarpc::util::{FirstSocketAddr, Never};
 
-service! {
-    rpc hello(name: String) -> String;
-}
-
-#[derive(Clone)]
-struct HelloServer;
-
-impl SyncService for HelloServer {
-    fn hello(&self, name: String) -> Result<String, Never> {
-        Ok(format!("Hello, {}!", name))
-    }
-}
+use tarpc::sync::{client, server,
+                  client::ClientExt};
+use owl_daemon::{
+    HelloServer,
+    SyncClient,
+    SyncServiceExt,
+};
 
 fn main() {
     let (tx, rx) = mpsc::channel();
     thread::spawn(move || {
-        let mut handle = HelloServer.listen("localhost:0", server::Options::default())
+        let handle = HelloServer.listen("localhost:0", server::Options::default())
             .unwrap();
         tx.send(handle.addr()).unwrap();
         handle.run();
     });
-    let client = SyncClient::connect(rx.recv().unwrap(), client::Options::default()).unwrap();
+    let addr = rx.recv().unwrap();
+    println!("{:?}", addr);
+    let client = SyncClient::connect(addr, client::Options::default()).unwrap();
     println!("{}", client.hello("Mom".to_string()).unwrap());
 }
