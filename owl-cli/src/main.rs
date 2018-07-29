7c0e474abe89537a2c26cad2a7c5254ddf0d3cca
Yechan Bae <qwazpia@gmail.com>
2018-07-29 21:06:26 +0900

    Setup clap-rs

--- owl-cli/src/main.rs
+++ owl-cli/src/main.rs
@@ -1,3 +1,19 @@
+#[macro_use]
+extern crate clap;
+
+use clap::{Arg, App, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Owl CLI")
+        .version("0.1")
+        .arg(Arg::with_name("config")
+            .short("c")
+            .long("config")
+            .value_name("TOML")
+            .help("Sets a custom config file (default is \"config.toml\"")
+            .takes_value(true))
+        .get_matches();
+
+    let config = matches.value_of("config").unwrap_or("config.toml");
+    println!("Value for config: {}", config);
 }
