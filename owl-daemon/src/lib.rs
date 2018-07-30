862ebe0895c6747129fc31e46ea543b60a3e79ed
Kangsu Kim <pica744@gmail.com>
2018-07-30 14:37:22 +0900

    Enable diesel

--- owl-daemon/src/lib.rs
+++ owl-daemon/src/lib.rs
@@ -1,23 +1,19 @@
 #![feature(plugin, use_extern_macros, proc_macro_path_invoc)]
 #![plugin(tarpc_plugins)]
 
-// Diesel ORM
-/*
-#[macro_use]
-extern crate diesel;
 extern crate chrono;
+#[macro_use] extern crate diesel;
+extern crate dotenv;
+#[macro_use] extern crate tarpc;
+
+use std::env;
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use dotenv::dotenv;
+use tarpc::util::Never;
 
 pub mod schema;
 pub mod models;
-
-use diesel::prelude::*;
-*/
-
-// tarpc
-#[macro_use]
-extern crate tarpc;
-
-use tarpc::util::Never;
 
 service! {
     rpc hello(name: String) -> String;
@@ -30,4 +26,16 @@ impl SyncService for HelloServer {
     fn hello(&self, name: String) -> Result<String, Never> {
         Ok(format!("Hello, {}!", name))
     }
+}
+
+pub fn connect_db() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = match env::var("DATABASE_URL") {
+        Ok(val) => val,
+        Err(_e) => "postgres://postgres@localhost/owl-daemon".to_string(),
+    };
+
+    PgConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
 }
