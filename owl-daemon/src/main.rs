4232bb3d0cb6fc3359a85a9f74a1682782e890ae
Kangsu Kim <pica744@gmail.com>
2018-07-30 15:04:19 +0900

    Add initial diesel skeleton code

--- owl-daemon/src/main.rs
+++ owl-daemon/src/main.rs
@@ -1,18 +1,45 @@
 extern crate owl_daemon;
 extern crate tarpc;
+extern crate diesel;
 
 use std::thread;
 
+use diesel::prelude::*;
 use tarpc::sync::server;
-use owl_daemon::{HelloServer, SyncServiceExt};
+use owl_daemon::{HelloServer, SyncServiceExt, connect_db, models, schema};
 
 fn main() {
-    let join_handle = thread::spawn(move || {
+    let server_handle = thread::spawn(move || {
         let handle = HelloServer.listen("localhost:5959", server::Options::default())
             .unwrap();
         println!("Owl-daemon is starting...");
         handle.run();
     });
 
-    join_handle.join().unwrap();
+    let db_handle = thread::spawn(move || {
+        use schema::teams;
+        use models::Team;
+
+        let pg_connection = connect_db();
+        diesel::insert_into(teams::table)
+            .values((teams::name.eq("PLUS"), teams::description.eq("Best Team")))
+            .execute(&pg_connection)
+            .unwrap();
+
+        let results = teams::table
+            .load::<Team>(&pg_connection)
+            .unwrap();
+
+        for team in results {
+            println!("{}: {}", team.name, team.description)
+        }
+
+        diesel::delete(teams::table)
+            .filter(teams::name.eq("PLUS"))
+            .execute(&pg_connection)
+            .unwrap();
+    });
+
+    db_handle.join().unwrap();
+    server_handle.join().unwrap();
 }
