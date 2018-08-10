f5d60ce9bf2f11a5d4588a6b1bfda8eb8d99f8d1
Kangsu Kim <pica744@gmail.com>
2018-08-11 05:46:30 +0900

    Auto update updated times in db models

--- owl-daemon/migrations/2018-07-23-061204_initialize_tables/down.sql
+++ owl-daemon/migrations/2018-07-23-061204_initialize_tables/down.sql
@@ -1,3 +1,8 @@
+DROP TRIGGER exploit_tasks_update_timestamp ON exploit_tasks;
+DROP TRIGGER exploits_update_timestamp ON exploits;
+DROP FUNCTION update_last_updated_time_at_column();
+DROP FUNCTION update_last_modified_time_at_column();
+
 DROP TABLE exploit_tasks;
 DROP TYPE exploit_status;
 DROP TABLE exploit_targets;
