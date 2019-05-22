c47dbc995266fda67eebcffc4c2573325e0f3512
Yechan Bae <qwazpia@gmail.com>
2019-05-22 23:06:41 +0900

    Implement URL routing

--- client/index.tsx
+++ client/index.tsx
@@ -1,65 +1,80 @@
 import React, { useState } from "react";
 import { Container, Menu, Icon } from "semantic-ui-react";
 import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";
 
 import Team from "./team";
 import User from "./user";
 
 const panes = [
-  { name: "Teams", render: () => <Team /> },
-  { name: "Users", render: () => <User /> }
+  { text: "Teams", url: "/teams", component: Team },
+  { text: "Users", url: "/users", component: User }
 ];
 
-const App: React.FC = () => {
-  const [active, setActive] = useState(panes[0].name);
+const App = () => {
+  const menu = panes.map(pane => {
+    const Component = withRouter(props => (
+      <Menu.Item
+        as={Link}
+        active={props.location.pathname.startsWith(pane.url)}
+        to={pane.url}
+      >
+        {pane.text}
+      </Menu.Item>
+    ));
 
-  const menu = panes.map(pane => (
-    <Menu.Item
-      key={pane.name}
-      as="a"
-      active={pane.name == active}
-      onClick={() => setActive(pane.name)}
-    >
-      {pane.name}
-    </Menu.Item>
+    return <Component key={pane.text} />;
+  });
+
+  const content = panes.map(pane => (
+    <Route key={pane.text} path={pane.url} component={pane.component} />
   ));
 
-  const content = panes
-    .filter(pane => pane.name == active)
-    .map(pane => pane.render())[0];
-
   return (
-    <div>
-      <Menu fixed="top" inverted>
-        <Container>
-          <Menu.Item header>
-            <Icon.Group
-              size="big"
-              style={{ marginRight: "0.5em", textAlign: "center" }}
-            >
-              <Icon color="yellow" size="large" name="circle" />
-              <Icon
-                color="black"
-                size="large"
-                name="circle"
-                style={{ marginLeft: "0.1em" }}
-              />
-              <Icon
-                circular
-                name="user secret"
-                style={{ marginLeft: "0.2em" }}
-              />
-            </Icon.Group>
-            Owl
-          </Menu.Item>
-          {menu}
-        </Container>
-      </Menu>
+    <Router>
+      <div>
+        <Menu fixed="top" inverted>
+          <Container>
+            <Menu.Item header>
+              <Icon.Group
+                size="big"
+                style={{ marginRight: "0.5em", textAlign: "center" }}
+              >
+                <Icon color="yellow" size="large" name="circle" />
+                <Icon
+                  color="black"
+                  size="large"
+                  name="circle"
+                  style={{ marginLeft: "0.1em" }}
+                />
+                <Icon
+                  circular
+                  name="user secret"
+                  style={{ marginLeft: "0.2em" }}
+                />
+              </Icon.Group>
+              Owl
+            </Menu.Item>
+            {menu}
+          </Container>
+        </Menu>
 
-      <Container text style={{ paddingTop: "7em" }}>
-        {content}
-      </Container>
-    </div>
+        <Container text style={{ paddingTop: "7em" }}>
+          <Route
+            path="/"
+            exact
+            component={() => <Redirect to={panes[0].url} />}
+          />
+          {content}
+        </Container>
+      </div>
+    </Router>
   );
 };
 
