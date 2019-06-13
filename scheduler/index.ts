0ab056860af10b6f0acaed284bd85ec3e8389353
Kangsu Kim <ksalpha.x@gmail.com>
2019-06-14 01:16:41 +0900

    Implement basic scheduling

---
+++ scheduler/index.ts
@@ -0,0 +1,55 @@
+import { synchdFn } from 'synchd';
+import { prisma, Task, TaskRunner } from "./generated/prisma-client";
+
+const oneMinute = 60 * 1000;
+
+async function schedule() {
+  const unscheduledTasks: Task[] = await prisma
+    .tasks({
+      where: {
+        runner: null
+      },
+      orderBy: "createdAt_ASC"
+    });
+
+  const idleRunners: TaskRunner[] = await prisma
+    .taskRunners({
+      where: {
+        idle: true
+      },
+      orderBy: "updatedAt_ASC"
+    });
+
+  const schedulingCount = Math.min(unscheduledTasks.length, idleRunners.length);
+
+  for (let i = 0; i < schedulingCount; i++) {
+    const task = unscheduledTasks[i];
+    const runner = idleRunners[i];
+    await prisma.updateTask({
+      where: {
+        id: task.id
+      },
+      data: {
+        runner: {
+          connect: {
+            id: runner.id
+          }
+        }
+      }
+    });
+    await prisma.updateTaskRunner({
+      where: {
+        id: runner.id
+      },
+      data: {
+        idle: false
+      }
+    });
+  }
+}
+
+const scheduleLock = {};
+const exclusiveSchedule = synchdFn(scheduleLock, schedule);
+
+// Maybe use subscribe?
+setInterval(exclusiveSchedule, oneMinute);
