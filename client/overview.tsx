6c9e3a501aadfc38b9be2dbc1911658f93588a6f
Yechan Bae <qwazpia@gmail.com>
2019-08-07 12:15:36 -0400

    Implement score timeline with static data

--- client/overview.tsx
+++ client/overview.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
 import { Header, Container, List } from "semantic-ui-react";
+import { Line } from "react-chartjs-2";
 
 import { prisma } from "../generated/prisma-client";
 import { Loader, useAwait } from "./common";
@@ -110,8 +111,7 @@ const Scoreboard: React.FC = () => {
     <Loader
       status={status}
       render={teamList => (
-        <Container text>
-          <Header as="h1">Scoreboard</Header>
+        <Container>
           <List divided relaxed ordered>
             {teamList.map(team => (
               <List.Item key={team.id}>
@@ -125,9 +125,57 @@ const Scoreboard: React.FC = () => {
   );
 };
 
+const ScoreTimeline: React.FC = () => {
+  const tension = 0.07;
+
+  let randomColorGenerator = function() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    return [
+      `rgba(${red},${green},${blue},0.7)`,
+      `rgba(${red},${green},${blue},0.3)`
+    ];
+  };
+
+  const colors = [];
+  for (let i = 0; i < 10; i++) {
+    colors.push(randomColorGenerator());
+  }
+
+  const data = {
+    labels: ["17:00", "17:05", "17:10"],
+    datasets: [
+      {
+        label: "PPP",
+        fill: false,
+        tension: tension,
+        borderColor: colors[0][0],
+        backgroundColor: colors[0][1],
+        data: [100, 150, 400]
+      },
+      {
+        label: "r00timentary",
+        fill: false,
+        tension: tension,
+        borderColor: colors[1][0],
+        backgroundColor: colors[1][1],
+        data: [200, 300, 270]
+      }
+    ]
+  };
+
+  return (
+    <Container>
+      <Line data={data} />
+    </Container>
+  );
+};
+
 export const Overview: React.FC = () => (
   <>
     <RoundDisplay />
     <Scoreboard />
+    <ScoreTimeline />
   </>
 );
