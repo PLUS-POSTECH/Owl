000f0177e2654d4f41c1a2d9b2522f0a0f7d1db5
Yechan Bae <qwazpia@gmail.com>
2019-08-09 14:39:20 -0700

    Deterministic scoreboard color

--- client/scoreboard.tsx
+++ client/scoreboard.tsx
@@ -18,20 +18,40 @@ const ScoreTimeline: React.FC = () => {
   const [addDefense, setAddDefense] = useState(true);
   const [addKoth, setAddKoth] = useState(true);
 
-  let randomColorGenerator = function () {
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
+  function HSVtoRGB(h: number, s: number, v: number) {
+    let r = 0, g = 0, b = 0, i, f, p, q, t;
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+      case 0: r = v, g = t, b = p; break;
+      case 1: r = q, g = v, b = p; break;
+      case 2: r = p, g = v, b = t; break;
+      case 3: r = p, g = q, b = v; break;
+      case 4: r = t, g = p, b = v; break;
+      case 5: r = v, g = p, b = q; break;
+    }
+    return {
+      r: Math.round(r * 255),
+      g: Math.round(g * 255),
+      b: Math.round(b * 255)
+    };
+  }
+
+  let colorGenerator = function (team_idx: number) {
+    const result = HSVtoRGB(team_idx / game_state.teams.length, 1, 0.7);
     return [
-      `rgba(${red},${green},${blue},0.7)`,
-      `rgba(${red},${green},${blue},0.3)`
+      `rgba(${result.r},${result.g},${result.b},0.7)`,
+      `rgba(${result.r},${result.g},${result.b},0.3)`
     ];
   };
 
   const teams_dict: { [id: string]: any } = {};
 
   for (const team of game_state.teams) {
-    let color = randomColorGenerator();
+    let color = colorGenerator(Number(team.id));
     teams_dict[team.id] = {
       id: team.id,
       label: team.name,
